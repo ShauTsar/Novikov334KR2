9aef7479b27d076f1fbe037c90e1a6cdffca69ff
User <user@401-10-Mac.local>
2022-11-09 09:57:43 +0500

    Initial commit

---
+++ app/src/main/java/com/infiren/novikov334kr2/LoadingActivity.kt
@@ -0,0 +1,11 @@
+package com.infiren.novikov334kr2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
